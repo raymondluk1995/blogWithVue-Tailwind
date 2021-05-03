358fe9e69252e12897471228ccc81b2a41df088c
raymondluk1995 <raymondluk1995@outlook.com>
2021-05-03 14:48:23 +1000

    m1

---
+++ sites/blog/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\Admin\DashboardController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware(['auth', 'verified'])
+    ->prefix('dashboard')
+    ->group(function(){
+        Route::get('/',[DashboardController::class,'index'])->name('dashboard');
+    });
