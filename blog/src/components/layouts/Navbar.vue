e73d95d99e33fdc4a8b948afb2dc30841ca23b86
raymondluk1995 <raymondluk1995@outlook.com>
2021-05-07 14:23:56 +1000

    part4 done

--- blog/src/components/layouts/Navbar.vue
+++ blog/src/components/layouts/Navbar.vue
@@ -1,70 +1,92 @@
 <template>
-  <nav
-    id="navbar"
-    class="px-4 py-4 sm:py-6 bg-white shadow sm:bg-gray-900 sm:shadow-none sm:fixed w-full z-10"
-  >
-    <div
-      class="container mx-auto flex flex-col sm:flex-row items-center justify-between"
-    >
-      <div class="w-full flex flex-row item-center justify-between">
+  <nav id="navbar"
+       class="px-4 py-4 sm:py-6 bg-white shadow sm:fixed w-full z-10 transition-colors duration-500"
+       :class="{ 'sm:bg-transparent sm:shadow-none': !showWhiteBackground }">
+    <div class="container mx-auto flex flex-col sm:flex-row items-center justify-between">
+      <div class="w-full flex flex-row items-center justify-between">
         <div class="text-3xl font-bold">
-          <span class="sm:text-white">Lara</span>
+          <span :class="{'sm:text-white': !showWhiteBackground}">Lara</span>
           <span class="text-primary">Tips</span>
         </div>
 
         <div>
-          <button v-show="!isVisible" class="sm:hidden" @click="toggle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+          <button v-show="!isVisible"
+                  class="sm:hidden"
+                  @click="toggle">
+            <svg class="w-6 h-6"
+                 fill="none"
+                 stroke="currentColor"
+                 viewBox="0 0 24 24"
+                 xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </button>
 
-          <button v-show="isVisible"  class="sm:hidden" @click="toggle">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          <button v-show="isVisible"
+                  class="sm:hidden"
+                  @click="toggle">
+            <svg class="w-6 h-6"
+                 fill="none"
+                 stroke="currentColor"
+                 viewBox="0 0 24 24"
+                 xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"></path>
             </svg>
           </button>
         </div>
       </div>
-      <div
-        class="sm:text-white mt-4 sm:mt-0 w-full sm:text-right sm:block sm:space-x-4"
-        :class="{'hidden': !isVisible}"
-      >
-        <NavbarLink to="/articles" label="Articles" />
-        <NavbarLink to="/about" label="About" />
-        <NavbarLink to="/contact" label="Contact" />
+      <div class="mt-4 sm:mt-0 w-full sm:space-x-4 sm:text-right sm:block"
+          :class="{ 'hidden': !isVisible, 'sm:text-white': !showWhiteBackground }">
+        <NavbarLink to="/"
+                    label="Home" />
+        <NavbarLink to="/articles"
+                    label="Articles" />
+        <NavbarLink to="/about"
+                    label="About" />
+        <NavbarLink to="/contact"
+                    label="Contact" />
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import NavbarLink from './NavbarLink.vue';
-import {userToggle} from '@/composables/userToggle';
+import NavbarLink from "./NavbarLink.vue";
+import { userToggle } from "@/composables/userToggle";
+import { ref } from 'vue';
+
 export default {
   components: {
     NavbarLink,
-    userToggle
   },
-  props:{},
+
+  props: {},
+
   setup() {
-    let {isVisible, toggle} = userToggle();
+    let { isVisible, toggle } = userToggle();
+    let showWhiteBackground = ref(false);
+
+    document.addEventListener("scroll", function () {
+      let bodyTopPosition = document.body.getBoundingClientRect().top;
+
+      if (bodyTopPosition < -150) {
+        showWhiteBackground.value = true;
+      } else {
+        showWhiteBackground.value = false;
+      }
+    });
 
     return {
       isVisible,
-      toggle
-    }
-  }
-}
+      toggle,
+      showWhiteBackground,
+    };
+  },
+};
 </script>
--- blog/src/views/Home/index.vue
+++ blog/src/views/Home/index.vue
@@ -1,17 +1,61 @@
 <template>
   <div>
     <Hero />
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente
+        ipsam veniam autem, asperiores doloremque aperiam distinctio dolorem
+        ullam tempora. Sit similique adipisci fugit possimus voluptatibus
+        aperiam optio, culpa consectetur exercitationem autem quaerat quam totam
+        fuga omnis corrupti harum unde dignissimos tempora dolorem suscipit
+        temporibus nisi? Maxime non totam voluptatibus. Est maiores ipsa
+        exercitationem distinctio soluta minima delectus animi ullam maxime ab
+        porro, labore obcaecati eveniet. Vel autem perspiciatis veritatis
+        provident consectetur, temporibus iste beatae vero, tempore sapiente
+        assumenda minus, soluta asperiores! Similique eum illum corrupti dolor
+        magnam corporis quia omnis autem, adipisci facere facilis doloribus
+        deserunt nulla, maxime et?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente
+        ipsam veniam autem, asperiores doloremque aperiam distinctio dolorem
+        ullam tempora. Sit similique adipisci fugit possimus voluptatibus
+        aperiam optio, culpa consectetur exercitationem autem quaerat quam totam
+        fuga omnis corrupti harum unde dignissimos tempora dolorem suscipit
+        temporibus nisi? Maxime non totam voluptatibus. Est maiores ipsa
+        exercitationem distinctio soluta minima delectus animi ullam maxime ab
+        porro, labore obcaecati eveniet. Vel autem perspiciatis veritatis
+        provident consectetur, temporibus iste beatae vero, tempore sapiente
+        assumenda minus, soluta asperiores! Similique eum illum corrupti dolor
+        magnam corporis quia omnis autem, adipisci facere facilis doloribus
+        deserunt nulla, maxime et?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis sapiente
+        ipsam veniam autem, asperiores doloremque aperiam distinctio dolorem
+        ullam tempora. Sit similique adipisci fugit possimus voluptatibus
+        aperiam optio, culpa consectetur exercitationem autem quaerat quam totam
+        fuga omnis corrupti harum unde dignissimos tempora dolorem suscipit
+        temporibus nisi? Maxime non totam voluptatibus. Est maiores ipsa
+        exercitationem distinctio soluta minima delectus animi ullam maxime ab
+        porro, labore obcaecati eveniet. Vel autem perspiciatis veritatis
+        provident consectetur, temporibus iste beatae vero, tempore sapiente
+        assumenda minus, soluta asperiores! Similique eum illum corrupti dolor
+        magnam corporis quia omnis autem, adipisci facere facilis doloribus
+        deserunt nulla, maxime et?
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Hero from "./Hero.vue"
+import Hero from "./Hero.vue";
 export default {
-  name: 'Home',
+  name: "Home",
 
   components: {
     Hero,
-  }
-}
+  },
+};
 </script>
