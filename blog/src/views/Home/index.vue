d6632c046bbf8965d417595f97d0605e7a4eece6
raymondluk1995 <raymondluk1995@outlook.com>
2021-05-07 13:54:32 +1000

    part3 done

---
+++ blog/src/views/Home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Hero />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hero from "./Hero.vue"
+export default {
+  name: 'Home',
+
+  components: {
+    Hero,
+  }
+}
+</script>
