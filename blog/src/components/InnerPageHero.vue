5b897364bf22ca70f5cc89742d9dc2a2ef2a129e
Raymond <raymondluk1995@outlook.com>
2021-05-10 14:15:28 +1000

    m1

---
+++ blog/src/components/InnerPageHero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="relative z-0 h-40 sm:h-64">
+    <img :src="imageUrl" alt="about" class="h-full w-full object-cover" />
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="absolute inset-0 bg-gradient-to-t from-primary opacity-50">
+      <div
+        class="container mx-auto sm:px-0 text-white h-full w-full flex flex-col items-center justify-center text-2xl sm:text-5xl sm:justify-end"
+      >
+        <div class="mb -4 sm:mb-8 text-center">{{ title }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        }
+    }
+};
+</script>
--- blog/src/views/About/index.vue
+++ blog/src/views/About/index.vue
@@ -1,21 +1,6 @@
 <template>
   <div>
-    <div class="relative z-0 h-40 sm:h-64">
-      <img
-        src="/images/image-2.jpg"
-        alt="about"
-        class="h-full w-full object-cover"
-      />
-      <div class="absolute inset-0 bg-black opacity-50"></div>
-      <div class="absolute inset-0 bg-gradient-to-t from-primary opacity-50">
-        <div
-          class="container mx-auto sm:px-0 text-white h-full w-full flex flex-col items-center justify-center text-2xl sm:text-5xl sm:justify-end"
-        >
-          <div class="mb -4 sm:mb-8 text-center">About us</div>
-        </div>
-      </div>
-    </div>
-
+    <InnerPageHero image-url="/images/image-2.jpg" title="About" />
     <div class="container mx-auto py-8 sm:py-12 px-4 sm:px-0">
         <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum voluptate voluptatem aperiam nobis? Tempora facilis natus ipsa quasi ut neque exercitationem voluptate porro dolores aut possimus magnam, vel autem explicabo culpa libero adipisci tenetur quibusdam eius? Quia, assumenda iste asperiores ducimus laborum, doloremque ipsam dignissimos ullam quo, culpa ex ipsa.
@@ -37,7 +22,12 @@
 </template>
 
 <script>
-export default {};
+import InnerPageHero from "@/components/InnerPageHero";
+export default {
+    components:{
+        InnerPageHero,
+    }
+};
 </script>
 
 <style>
