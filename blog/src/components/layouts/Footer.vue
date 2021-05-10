d56d9f8081841269d7db28af326439e6044339d8
raymondluk1995 <raymondluk1995@outlook.com>
2021-05-10 11:38:11 +1000

    part 7 done

--- blog/src/components/layouts/Footer.vue
+++ blog/src/components/layouts/Footer.vue
@@ -1,13 +1,15 @@
 <template>
-  <footer class="text-center py-6 sm:py-8 bg-black text-white text-base">
-      <div class="container mx-auto px-4 sm:px-0">
-          <span>Copyright &copy 2021 All rights reserved</span>
-      </div>
+  <footer class="text-center py-6 sm:py-12 bg-black text-white text-base">
+    <div class="container mx-auto px-4 sm:px-0">
+      <span>Copyright &copy;2021 All rights reserved.</span>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
+  props: {},
 
-}
+  setup() {},
+};
 </script>
